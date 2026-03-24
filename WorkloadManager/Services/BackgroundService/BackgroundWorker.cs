6c6469d425fe18cd3b939c02ac7bc5ed0a75c6f8
Asher <ashi.berg@gmail.com>
2026-03-24 16:46:34 +0200

    added background service

---
+++ WorkloadManager/Services/BackgroundService/BackgroundWorker.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Hosting;
+
+namespace WorkloadManager.Services.BackgroundService
+{
+    public class BackgroundWorker : Microsoft.Extensions.Hosting.BackgroundService
+    {
+        private readonly string _instanceId;
+
+        public BackgroundWorker()
+        { 
+            _instanceId = Guid.NewGuid().ToString();
+        }
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
